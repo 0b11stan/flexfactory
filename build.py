692418130260e37895ca074f4970842efd2e4dd4
Tristan Pinaudeau <tristan@tic.sh>
2021-06-28 17:11:31 +0200

    debt: use pure html instead of js

--- build.py
+++ build.py
@@ -1,20 +1,44 @@
 #!/bin/python
 
 import json
-from jinja2 import Template
+from jinja2 import FileSystemLoader, Environment
 
-with open('templates/index.html.j2', 'r') as file:
-    template = file.read()
+loader = FileSystemLoader("templates")
 
-with open('data/background.json', 'r') as file:
-    background = json.loads(file.read())
 
-with open('data/readings.json', 'r') as file:
-    readings = json.loads(file.read())
+def build_page(template, **data):
+    return Environment(loader=loader).from_string(template).render(data)
 
+
+def read_file(basedir, name):
+    with open(f'{basedir}/{name}', 'r') as file:
+        if '.json' in name:
+            return json.loads(file.read())
+        return file.read()
+
+
+def read_data(name):
+    return read_file('data', f'{name}.json')
+
+
+def read_template(name):
+    return read_file('templates', f'{name}.html.j2')
+
+
+background = read_data('background')
+readings = read_data('readings')
+articles = read_data('articles')
+
+with open('www/readings.html', 'w') as output:
+    output.write(build_page(read_template('readings'),
+                            background=background,
+                            readings=readings))
+
+with open('www/articles.html', 'w') as output:
+    output.write(build_page(read_template('articles'), background=background,
+                            articles=articles))
+
+# TODO : just use a redirect on proxy
 with open('www/index.html', 'w') as output:
-    output.write(Template(template).render(
-        background=background,
-        readings=readings
-    ))
-
+    output.write(build_page(read_template('articles'), background=background,
+                            articles=articles))
